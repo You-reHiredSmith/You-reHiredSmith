8a6bf1d4084390d299e22b2627341d68c17c5db8
Katherine <80862932+katherinek123@users.noreply.github.com>
2022-06-25 14:45:06 -0700

    Table setup

---
+++ src/components/Table.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+
+export default function Table() {
+
+  return (
+    <table>
+      <th>Company Name ID</th>
+      <th>Status</th>
+      <th>Priority</th>
+      <th>Notes</th>
+      <th>Posting</th>
+    </table>
+  )
+}

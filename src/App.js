74cfb7487431ecd92689d7ec2c6ee458eda2f3d3
theharrylyon <tannerhlyon@gmail.com>
2022-06-27 11:09:20 -0700

    Created documents folder/page. Updated App.js to import Documents.jsx and added route to Documents.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store from './store'
 import { Provider } from 'react-redux'
 import Homepage from "./pages/Homepage/Homepage.jsx";
+import Documents from "./pages/Documents/Documents.jsx";
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-
+          <Route path="/" element={<Documents />} />
         </Routes>
       </BrowserRouter>
     </Provider>

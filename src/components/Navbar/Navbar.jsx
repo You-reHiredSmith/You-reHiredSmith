63097bf094f6ea023c5e723562333c6f455e8a64
Katherine <80862932+katherinek123@users.noreply.github.com>
2022-06-27 16:55:49 -0700

    not finished changes for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import img from '../../img/Hiresmith23.png'
 
-export default function Navbar () {
+export default function Navbar() {
   return (
     <div id="div">
       <nav id="navbar">
+        <img src={img}></img>
         <button className="homepage">
           <Link to='/'>
             <p>Home</p>
@@ -17,13 +19,13 @@ export default function Navbar () {
           </a>
         </button>
         <button className="apphistory">
-            < a href="#">
-              Application History
-            </a>
+          < a href="#">
+            Application History
+          </a>
         </button>
         <button className="docs">
           <Link to='/documents'>
-              <p>Docs</p>
+            <p>Docs</p>
           </Link>
         </button>
       </nav>

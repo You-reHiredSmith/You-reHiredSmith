903b9873fbee0faaad5bb3b4c74230b4d840831f
James <jamesmbui1999@gmail.com>
2022-06-27 16:12:56 -0700

    Added navbar to docs and added to react router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 export default function Navbar () {
   return (
     <div id="div">
       <nav id="navbar">
         <button className="homepage">
-          < a href="#">
-            Home
-          </a>
+          <Link to='/'>
+            <p>Home</p>
+          </Link>
         </button>
         <button className="joblist" >
           <a href="#">
@@ -16,14 +17,14 @@ export default function Navbar () {
           </a>
         </button>
         <button className="apphistory">
-          < a href="#">
-            Application History
-          </a>
+            < a href="#">
+              Application History
+            </a>
         </button>
         <button className="docs">
-          < a href="#">
-            Docs
-          </a>
+          <Link to='/documents'>
+              <p>Docs</p>
+          </Link>
         </button>
       </nav>
     </div>

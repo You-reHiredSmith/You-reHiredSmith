89ae59b8a87243158d71f18fac6daf1b1aaa4f48
Ryan Huie <rhpurple1023@gmail.com>
2022-06-30 17:09:05 -0700

    Completed dynamic row color + in progress Document file submission

--- src/pages/Documents/Documents.jsx
+++ src/pages/Documents/Documents.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar.jsx'
+import { useState } from 'react'
 
 export default function Documents () {
   // there's nothing really going on here
+
+  const [fileName, setFileName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null)
+
+  const onFileUpload = () => {
+
+  }
+
   return (
     <div>
-      <Navbar />
-      <h1> Documents Page </h1>
+      <div>
+        <Navbar />
+      </div>
+      <div id="uploadFile">
+        <form>  
+          <input
+            type="text"   
+            value={fileName}
+            onChange={(e) => setFileName(e.target.value)}
+            />
+          <input
+            type="file"
+            value={selectedFile}
+            onChange={(e) => setSelectedFile(e.target.files[0])}
+            />
+        </form>
+      </div>
     </div>
   )
 }
